e9ee718cf560f3f0aee94cfa40c6425ea88da81b
Maha Ali <mahaali@Mahas-MacBook-Air.local>
2024-09-22 20:41:46 -0400

    Updates

---
+++ precache-manifest.9ab943bf40a3bf494def71bed53b1457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8671846b52357ebad45c8ccfe59a90c",
+    "url": "/RanoushVilla/index.html"
+  },
+  {
+    "revision": "d9d3a7c72ce441911826",
+    "url": "/RanoushVilla/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a0507e5eb3362f416b33",
+    "url": "/RanoushVilla/static/js/2.596523c0.chunk.js"
+  },
+  {
+    "revision": "d9d3a7c72ce441911826",
+    "url": "/RanoushVilla/static/js/main.a084adef.chunk.js"
+  },
+  {
+    "revision": "ac73db5f5fab1c45d8bb",
+    "url": "/RanoushVilla/static/js/runtime~main.67a119ab.js"
+  }
+]);
